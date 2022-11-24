8d79d54370c7e499388e74c5d008935b66628445
Snackman8 <lawy888@gmail.com>
2022-11-23 23:50:47 -0800

    reduce skip to 4 percent

--- paneTakingQuiz.py
+++ paneTakingQuiz.py
@@ -232,7 +232,7 @@ def init_pane(jsc, quiz_id, user_id, **kwargs):
     jsc.tag["WRONG"] = 0
     jsc.tag["REMEDIAL"] = 0
     jsc.tag["QUIZ_FLAGS"] = quiz['flags']
-    jsc.tag["SKIPS_LEFT"] = max(1, int(len(jsc.tag['QUESTIONS_REMAINING']) / 10))
+    jsc.tag["SKIPS_LEFT"] = max(1, int(len(jsc.tag['QUESTIONS_REMAINING']) / 25))
 
     # start the quiz stat
     if jsc.tag['QUIZ_TYPE'] == 'Mini':
